a07c4398487c623ffadf6563f85efd8707e1cd0b
Ross Chapman <rosschapman@gmail.com>
2017-12-20 23:43:39 -0800

    Hotfix for dep_track.sh

--- bin/dep_track.sh
+++ bin/dep_track.sh
@@ -2,21 +2,27 @@
 
 # vars
 FILE_NAME="README.md"
-UI_DELAY=.5
+UI_DELAY=1
 
 # Get list of 
 echo "Retrieving dependency list and calculating length..." 
 dep_len=$(npm ls --json --dev --depth=0 | jq length)
 echo "Looks like we got ${dep_len}"
-UI_sleep DELAY
+sleep $UI_DELAY
 echo "Checking if it's changed since last time..."
 if grep -Fq "devDependencies is ${dep_len}" $FILE_NAME
 then
+  sleep $UI_DELAY
   echo "Nothing's changed, goodbye!!"
   exit
 else
-  echo "Writing to ${FILE_NAME}..." 
-  UI_sleep DELAY
+  sleep $UI_DELAY
+  # TODO: add # of diff, ie "meh, looks like we added n more" or "yay, we 
+  # actually removed some!"
+  echo "Yep, looks we like added some."
+  sleep $UI_DELAY
+  echo "Writing the new value to ${FILE_NAME}..." 
+  sleep $UI_DELAY
   sed -i "" "s|devDependencies is [0-9]|devDependencies is ${dep_len}|g" $FILE_NAME
   echo "All done!!"
 fi
